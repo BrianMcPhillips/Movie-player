91076d2c64d685bf19fcbf9bb8b6c460ecb8dfa7
Brian McPhillips <B.McPhillips89@gmail.com>
2021-01-28 14:05:10 -0800

    video with time update

--- script.js
+++ script.js
@@ -16,15 +16,15 @@ function toggleVideoStatus() {
 }
 
 function updatePlayIcon() {
-  if(videopaused) {
-    play.innerHTML = '<i class="fa fa-play fa-2x></i>';
+  if(video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
-    play.innerHTML = '<i class="fa fa-pause fa-2x></i>';
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
 
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
 }
 
 function setVideoProgress() {
@@ -41,7 +41,7 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('stop', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
